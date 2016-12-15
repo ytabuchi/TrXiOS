15c91714572cdfe9a9a780ebff5e6024a117742e
ytabuchi @ Surface <ytabuchi.xlsoft@gmail.com>
2016-12-15 20:50:59 +0900

    Clean storyboard

--- Start/UITableViewSample/UITableViewSample.iOS/ViewController.designer.cs
+++ Start/UITableViewSample/UITableViewSample.iOS/ViewController.designer.cs
@@ -14,25 +14,8 @@ namespace UITableViewSample.iOS
     [Register ("ViewController")]
     partial class ViewController
     {
-        [Outlet]
-        [GeneratedCode ("iOS Designer", "1.0")]
-        UIKit.UITableView CustomTableView { get; set; }
-
-        [Outlet]
-        [GeneratedCode ("iOS Designer", "1.0")]
-        UIKit.UIButton GetSpeakersButton { get; set; }
-
         void ReleaseDesignerOutlets ()
         {
-            if (CustomTableView != null) {
-                CustomTableView.Dispose ();
-                CustomTableView = null;
-            }
-
-            if (GetSpeakersButton != null) {
-                GetSpeakersButton.Dispose ();
-                GetSpeakersButton = null;
-            }
         }
     }
 }
