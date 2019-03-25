eeb99a0529ebf6eb12841294815576a525a83c84
Yoshito Tabuchi <ytabuchi.xlsoft@gmail.com>
2019-03-25 14:33:40 +0900

    Update Icon settings

--- Start/UITableViewSample/UITableViewSample/UITableViewSample/Models/SpeakersModel.cs
+++ Start/UITableViewSample/UITableViewSample/UITableViewSample/Models/SpeakersModel.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
-using System.Net.Http;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
